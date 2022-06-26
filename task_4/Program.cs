000a4a983ff118eb3edbdfd1d5270326bc90fb6b
Maria Zernina <ArishchinaMV@yandex.ru>
2022-06-26 19:18:05 +0700

    task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое значение ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе значение ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье значение ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
