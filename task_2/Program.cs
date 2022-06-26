c56a13bfcd0658d53b3b1b0ab4ebea77705de0b1
Maria Zernina <ArishchinaMV@yandex.ru>
2022-06-26 18:56:05 +0700

    task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое значение ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе значение ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write("Максимальное значение ");
+    Console.WriteLine(numberA);
+} 
+else
+{
+    Console.Write("Максимальное значение ");
+    Console.WriteLine(numberB);
+}
