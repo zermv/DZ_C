bdc9f438e63b1ccb2d5ac8282780a2201ef53778
Maria Zernina <ArishchinaMV@yandex.ru>
2022-07-02 13:52:10 +0700

    task 13

---
+++ task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите значение ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100)
+Console.WriteLine("Нет третьей цифры");
+else 
+{
+    while (number > 1000)
+    {
+        number = number/10;
+    }
+Console.WriteLine(number%10);
+}
+
+
