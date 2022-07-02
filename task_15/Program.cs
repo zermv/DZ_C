30037368a3f2508a4c1fd6c887e8d23671589482
Maria Zernina <ArishchinaMV@yandex.ru>
2022-07-02 14:16:44 +0700

    task 15

---
+++ task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number < 1) || (number > 7))
+{
+    Console.WriteLine("Нет такого дня недели");
+}
+else
+{
+    if (number > 5)
+    Console.WriteLine("Выходной день");
+    else 
+    Console.WriteLine("Будний день");
+
+}
