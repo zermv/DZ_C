a9c507afbfd45fd27ff4252e801704a9311a0150
Maria Zernina <ArishchinaMV@yandex.ru>
2022-07-05 00:31:00 +0700

    task 25

---
+++ task_25/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое число");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Math.Pow(n1, n2));
+
