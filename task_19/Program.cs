779f4149bc1937588eacdac56d0286ef601ef55e
Maria Zernina <ArishchinaMV@yandex.ru>
2022-07-04 23:52:28 +0700

    task 19

---
+++ task_19/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int numberA = n%10;
+int numberB = n/10%10;
+int numberC = n/1000%10;
+int numberD = n/10000;
+if (numberA == numberD && numberB == numberC)
+    Console.WriteLine("Палиндром");
+else 
+    Console.WriteLine("Не палиндром");
